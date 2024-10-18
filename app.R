8517429e118a404ef01929a0941beeb08025682a
mikael04 <mikael.coletto.eng@gmail.com>
2024-10-18 09:19:26 -0300

    Ajustando app.R;

--- app.R
+++ app.R
@@ -2,8 +2,9 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
-env_settings <-  utils_environment_config() ##utils_environtment
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
+source("R/utils_environment.R")
+env_settings <- utils_environment_config() ##utils_environtment
 options( "golem.app.prod" = env_settings$prod)
 options(shiny.port = env_settings$shiny_port)
 shiny.ef.aps.dashboard::run_app() # add parameters here (if any)

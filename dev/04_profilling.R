277206c7c8d89243c6463dfc1a8d1922aeda308e
mikael04 <mikael.coletto.eng@gmail.com>
2024-10-29 07:25:06 -0300

    Adicionando script para profilling;

---
+++ dev/04_profilling.R
@@ -0,0 +1,27 @@
+# Setup Golem app -----
+# Set options here
+options(golem.app.prod = FALSE) # TRUE = production mode, FALSE = development mode
+
+# Comment this if you don't want the app to be served on a random port
+# options(shiny.port = httpuv::randomPort())
+options(shiny.port = 4000)
+
+# Detach all loaded packages and clean your environment
+golem::detach_all_attached()
+# rm(list=ls(all.names = TRUE))
+
+# Document and reload your package
+golem::document_and_reload()
+
+# Reactlog ----
+## tell shiny to log all reactivity
+reactlog_enable()
+
+# Run app
+run_app()
+
+# once app has closed, display reactlog from shiny
+shiny::reactlogShow()
+
+# Profvis ----
+profvis::profvis(print(run_app()))

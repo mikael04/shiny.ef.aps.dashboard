3d81b74884de7cd71abbd32b14bbf512668e2f0f
Mikael M Coletto <mikael.coletto.eng@gmail.com>
2025-02-17 16:38:17 -0300

    Atualizando app para versão de debug do servidor;

--- app.R
+++ app.R
@@ -2,13 +2,27 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
+
+## Lidando com argumentos
+args <- commandArgs(trailingOnly = TRUE)
+## Rodando Shiny
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
 source("R/utils_environment.R")
-print(paste0("Working directory = ", getwd()))
-env_settings <- utils_environment_config() ##utils_environtment
-print(paste0("Env = ", Sys.getenv("R_CONFIG_ACTIVE")))
-print(paste0("Prod = ", env_settings$prod))
-print(paste0("Port = ", env_settings$port))
-options( "golem.app.prod" = env_settings$prod)
-options(shiny.port = env_settings$port)
+# print(paste0("Working directory = ", getwd()))
+## Buscando configurações do ambiente
+env_settings <- utils_environment_config()
+
+## Se estiver rodando em modo dev, setar a porta pelo argumento da linha de comando
+if(shiny::isTruthy(args) && args[1] == "dev"){
+  print("app.R in dev mode")
+  print(paste0("args = ", args))
+  options( "golem.app.prod" = env_settings$prod)
+  options(shiny.port = as.integer(args[2]))
+} else {
+  print(paste0("Env = ", Sys.getenv("R_CONFIG_ACTIVE")))
+  print(paste0("Prod = ", env_settings$prod))
+  print(paste0("Port = ", env_settings$port))
+  options( "golem.app.prod" = env_settings$prod)
+  options(shiny.port = env_settings$port)
+}
 shiny.ef.aps.dashboard::run_app() # add parameters here (if any)
